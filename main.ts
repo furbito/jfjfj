97a07d50f255801c1a06dbfc3fbb252ae0a03d7c
furbito <122534443+furbito@users.noreply.github.com>
2023-01-31 13:49:36 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,72 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    while (Enivado == 0) {
+        basic.pause(100)
+    }
+    basic.clearScreen()
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (receivedNumber == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+    radio.sendNumber(3)
+    Enivado = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    radio.sendNumber(1)
+    Enivado = 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    radio.sendNumber(2)
+    Enivado = 1
+})
+let Enivado = 0
+radio.setGroup(10)
+Enivado = 0
+basic.forever(function () {
+	
+})
